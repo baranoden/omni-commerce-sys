e39baaacaf784a552046feabf08e412b34a75658
Baran Öden <baranoden@gmail.com>
2026-06-02 11:24:04 +0300

    project init

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Req() req: any) {
+    return req.user;
+  }
+}
